c629e743cabfc14f43781a76155dd3a39db81a19
emmanuelcheruiyot-oss <emmanuel.cheruiyot4@student.moringaschool.com>
2026-04-27 18:10:12 +0300

    Arrow functions

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
-
+const createLoginTracker = (userInfo) => {
+  let attemptCount = 0;
+  return (passwordAttempt) => {
+    attemptCount++; 
+    if (attemptCount > 3) {
+      return "Account locked due to too many failed login attempts";
+    }
+    if (passwordAttempt === userInfo.password) {
+      return "Login successful";
+    }
+    return `Login attempt ${attemptCount}: Login failed`;
+  };
+};
 
 
 module.exports = {
   ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
 };
+
